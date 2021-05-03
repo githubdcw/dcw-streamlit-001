e5a2639c00121945d344f523f855121a2f217b95
githubdcw <36253415+githubdcw@users.noreply.github.com>
2021-05-03 14:23:25 +0700

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -9,3 +9,9 @@ st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
