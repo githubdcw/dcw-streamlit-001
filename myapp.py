c474185bb5daa13b9d2a1dd0a1d491da69208d9a
githubdcw <36253415+githubdcw@users.noreply.github.com>
2021-05-03 14:21:00 +0700

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,3 +3,9 @@ import streamlit as st
 # working with sample data.
 import numpy as np
 import pandas as pd
+st.title('My first app')
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
