d0b806cbe6f039d40446e4635908c135851b01d3
Denchai Worasawate <36253415+githubdcw@users.noreply.github.com>
2021-10-25 22:57:52 +0700

    Update testapp.py

--- testapp.py
+++ testapp.py
@@ -2,3 +2,8 @@ import streamlit as st
 from mylib import *
 st.write(add(1,2))
 st.write('akira')
+if st.button('Say hello'):
+...     st.write('Hi Akira')
+... else:
+...     st.write('Goodbye AK')
+
