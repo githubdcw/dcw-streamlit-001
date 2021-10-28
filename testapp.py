b96ba373e76e6cc4b5eaa7ace6df714b29e48cd3
Denchai Worasawate <36253415+githubdcw@users.noreply.github.com>
2021-10-28 22:11:58 +0700

    Update testapp.py

--- testapp.py
+++ testapp.py
@@ -1,9 +1,26 @@
 import streamlit as st
 from mylib import *
+def xxx(a,c):
+  if a == 0:
+    c.write('1')
+    a = 1
+  else:
+    c.write('0')
+    a = 0
+  return    
+
+c = st.contrainer()
+a=0
+st.button('Click',on_click=xxx,kwargs={
+        'a': a, 'c': c
+    })
+
 st.write(add(1,2))
 st.write('akira')
 if st.button('Say hello'):
   st.write('Hi Akira')
 else:
   st.write('Goodbye AK')
+  
+  
 
