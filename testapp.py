b0c4ce5caf4a1e079d046716a27f61e198d9ab7e
Denchai Worasawate <36253415+githubdcw@users.noreply.github.com>
2021-10-28 23:22:13 +0700

    Update testapp.py

--- testapp.py
+++ testapp.py
@@ -1,23 +1,46 @@
 import streamlit as st
 from mylib import *
-def xxx(a,b,c):
-  st.write(b)
-  b= b+1
+import dataclasses
+def persistent_game_state(initial_state):
+    session_id = st.report_thread.get_report_ctx().session_id
+    session = st.server.server.Server.get_current()._get_session_info(session_id).session
+    # object2 = object1
+    # modify object2 then object is modified as well
+    # They are mapped to the same memory.
+    st.write('in presistent')
+    if not hasattr(session, '_gamestate'):
+        st.write('no gamestate')
+        setattr(session, '_gamestate', initial_state)
+    else:
+        st.write('has gamestate')
+        st.write(st.server.server.Server.get_current()._get_session_info(session_id).session._gamestate)
+        st.write(session._gamestate)
+
+    return session._gamestate
+def xxx(a,c):
   if a == 0:
     a = 1
     c.empty()
     c.text(a)
   else:
+    a = 0
     c.empty()
     c.text(a)
   return    
 
+@dataclasses.dataclass
+class GameState:
+    number: int
+    num_guesses: int = 0
+    game_number: int = 0
+    game_over: bool = False
+
+state = persistent_game_state(initial_state=GameState(0))
+
 c = st.empty()
-a=0
-b=0
-c.text(a)
+c.text(state.number)
 st.button('Click',on_click=xxx,kwargs={
-        'a': a, 'b':b, 'c': c
+        'a': state.number,  'c': c
     })
 
 st.write(add(1,2))
