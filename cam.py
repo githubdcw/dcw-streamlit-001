a894dae7d64b719866a5f891adf516004457a1a6
Denchai Worasawate <36253415+githubdcw@users.noreply.github.com>
2022-07-23 23:54:48 +0700

    Update cam.py

--- cam.py
+++ cam.py
@@ -1,11 +1,11 @@
 import streamlet as st
-import cv2
+# import cv2
 st.title('Webcam Test')
 run = st.checkbox('Run')
-FRAME_WINDOW = st.image([])
-cap = cv2.VideoCapture(1)
-while run:
-  ret, frame = cam.read()
-  FRAME_WINDOW.image(frame)
-else:
-  st.write('STOPPED')
+# FRAME_WINDOW = st.image([])
+# cap = cv2.VideoCapture(1)
+# while run:
+#   ret, frame = cam.read()
+#   FRAME_WINDOW.image(frame)
+# else:
+#   st.write('STOPPED')
