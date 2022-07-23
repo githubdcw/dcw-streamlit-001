8f41277cb98588841b45b86f847559bdd8646894
Denchai Worasawate <36253415+githubdcw@users.noreply.github.com>
2022-07-24 00:50:01 +0700

    Update cam.py

--- cam.py
+++ cam.py
@@ -3,9 +3,23 @@ from streamlit_webrtc import webrtc_streamer
 import cv2
 st.title('Webcam Test')
 run = st.checkbox('Run')
+
+import av
+
+
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    flipped = img[::-1,:,:]
+
+    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
+
+
+webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
+
 # FRAME_WINDOW = st.image([])
 # cap = cv2.VideoCapture(1)
-webrtc_streamer(key="example")
+# webrtc_streamer(key="example")
 # while run:
 #   webrtc_streamer(key="example")
 #   ret, frame = cam.read()
